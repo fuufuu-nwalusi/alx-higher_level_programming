3e8206319ba775de86fadb366db89f215a71eebb
Epique Buff <oliviaujunwa851@gmail.com>
2024-01-16 13:22:00 -0800

    Alphabet Game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
-# Ifunanya Nwalusiuka 
-
-"""Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+    print("{:c}".format(letter), end="")
